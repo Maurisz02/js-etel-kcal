a42ebd3a49e7b320e8c48ee390018713379c62be
Maurisz <p2002adam@gmail.com>
2022-10-04 12:23:44 +0200

    alap kesz

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 let food = [
+    
+]
 
-];
+
+let osszKcal = 0;
+
+let kcalSzamlalo = document.getElementById('kcal');
 
 function addPizza(){
-
+    let szoveg = "Pizza (800 kcal)"
+    osszKcal += 800;
+    let list = document.getElementById('lista');
+    let listElem = document.createElement('li');
+    listElem.classList.add('list-group-item');
+    listElem.textContent = szoveg;
+    list.append(listElem); 
+    kcalSzamlalo.textContent = "Össz Kalória: "+ osszKcal;
 }
 
 function addKfc(){
-
+    let szoveg = "Kfc (1000 kcal)"
+    osszKcal += 1000;
+    let list = document.getElementById('lista');
+    let listElem = document.createElement('li');
+    listElem.classList.add('list-group-item');
+    listElem.textContent = szoveg;
+    list.append(listElem); 
+    kcalSzamlalo.textContent = "Össz Kalória: "+ osszKcal;
 }
 
 function addMonster(){
-    
+    let szoveg = "Arany Monster (350 kcal)"
+    osszKcal += 350;
+    let list = document.getElementById('lista');
+    let listElem = document.createElement('li');
+    listElem.classList.add('list-group-item');
+    listElem.textContent = szoveg;
+    list.append(listElem); 
+    kcalSzamlalo.textContent = "Össz Kalória: "+ osszKcal;
 }
+
 
 
 function init (){
